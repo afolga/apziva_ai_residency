c78a4dfd56f7a8a0c52f53de46bfa6bcc90e339e
afolga <85518452+afolga@users.noreply.github.com>
2023-11-28 20:42:54 -0600

    train, inference

--- project1/src/train.py
+++ project1/src/train.py
@@ -1,29 +1,56 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import numpy as np 
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, classification_report, f1_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score, make_scorer
+from sklearn.model_selection import KFold, cross_val_score
+from imblearn.under_sampling import RandomUnderSampler  
 
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.feature_selection import mutual_info_classif, chi2
-from sklearn.metrics import f1_score, accuracy_score, recall_score, precision_score, confusion_matrix, classification_report
-import pickle
+data=pd.read_csv("C:\\Users\\agnes\\Documents\\apziva_ai_residency\\project2\\data\\term-deposit-marketing-2020.csv")
 
 
-data=pd.read_csv("C:\\Users\\agnes\\Documents\\apziva_ai_residency\\project1\\Data\\ACME-HappinessSurvey2020.csv")
+# Data encoding
+data['job']=data['job'].astype('category')
+data['marital']=data['marital'].astype('category')
+data['education']=data['education'].astype('category')
+data['default']=data['default'].astype('category')
+data['housing']=data['housing'].astype('category')
+data['loan']=data['loan'].astype('category')
+data['contact']=data['contact'].astype('category')
+data['month']=data['month'].astype('category')
+data['y']=data['y'].astype('category')
+cat_columns = data.select_dtypes(['category']).columns
+data[cat_columns] = data[cat_columns].apply(lambda x: x.cat.codes)
+ # not using loan or default 
+x  = data[['age', 'job','marital','education','balance','housing','day','month','duration','campaign']]
+y = data[['y']]
 
-# Prepare data
-y = data["Y"]
-X = data.drop(columns = ['Y'])
-X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.3, random_state=12)
+x_train,x_test,y_train,y_test=train_test_split(x, y, test_size=0.2, random_state=0)
 
+# under sampling to account for data imbalance 
+under_sampler = RandomUnderSampler()
+X_res, y_res = under_sampler.fit_resample(x, y)
+x_train_res, y_train_res=under_sampler.fit_resample(x_train,y_train)
 
-dt = DecisionTreeClassifier(random_state=0, ccp_alpha=0.016)
-dt.fit(X_train,Y_train)
+# time for grid search and logistic regression 
+
+f1 = make_scorer(f1_score , average='macro')
+grid={"C":np.logspace(-3,3,7), "penalty":["l1","l2"]}# l1 lasso l2 ridge
+logreg=LogisticRegression()
+logreg_cv=GridSearchCV(logreg,grid,cv=10,scoring=f1)
+logreg_cv.fit(x_train_res, y_train_res.values.ravel())
+
 
 # Evaluation
-pred_test = dt.predict(X_test)
-accuracy_score(Y_test, pred_test)
+logreg2=LogisticRegression(C=0.01,penalty="l2")
+logreg2.fit(x_train_res, y_train_res.values.ravel())
 
-
+predictions=lr2.predict(x_test)
+f1_score(y_test,predictions)
 
 with open('trained_model.pkl', 'wb') as f:
     pickle.dump(dt, f)
